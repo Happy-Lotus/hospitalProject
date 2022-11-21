b61e546095b5c48f14d6b3c7ba7db81b768f597e
HeeYeon-Kim <62700196+HeeYeon-Kim@users.noreply.github.com>
2022-11-22 05:48:15 +0900

    Update Reception.java

--- src/hospital/Reception.java
+++ src/hospital/Reception.java
@@ -1,9 +1,10 @@
 package hospital;
 import facade.UIData;
 import mgr.Manageable;
-import java.util.Scanner;
+import java.util.*;
 
 public class Reception implements Manageable, UIData {
+
     String date;
     Patient patient;
     String name;
@@ -16,13 +17,16 @@ public class Reception implements Manageable, UIData {
     public void read(Scanner scan) {
         date = scan.next();
         String patientCode = scan.next();
-        patient = (Patient)Main.patientMgr.find(patientCode);
+        patient = (Patient) Main.patientMgr.find(patientCode);
         if (patient == null) {
-            System.out.println("환자 비어있음");
+            System.out.println("해당 환자코드와 일치하는 환자가 없습니다. 등록이 필요합니다.");
+            patient = new Patient();
+            patient.read(scan);
         }
         name = scan.next();
-        gender = patient.gender;
-        birth = patient.birth;
+        gender = scan.next();
+
+        birth = patient.getBirth();
         String temp;
         while(true){
             temp = scan.next();
@@ -31,18 +35,37 @@ public class Reception implements Manageable, UIData {
             }
             symptom+=temp+" ";
         }
+
+
         doctor = (Doctor)Main.doctorMgr.find(scan.next());
 
         if (doctor == null) {
-            System.out.println("일치하는 의사가 없음");
-            System.exit(0);
+            Random random = new Random();
+            System.out.println("일치하는 의사가 없습니다. 의사를 자동 배정합니다.");
+            int index = random.nextInt(Main.doctorMgr.mList.size());
+            doctor = (Doctor)Main.doctorMgr.getMlist().get(index);
         }
 
         if(patient.matches(patientCode)) {//신규환자일 경우 의사가 담당하는 patientList에 저장함. 아닐 경우 pass.
             doctor.addPatient(patient);
         }
-        if(patient.matches(patientCode)) {//신규환자일 경우 의사가 담당하는 patientList에 저장함. 아닐 경우 pass.
+        if(patient.matches(patientCode)) {//환자코드와 patient가 일치할 경우 patient의 진료기록에 현재 진료기록을 저장함.
             patient.addReception(this);
+        }
+        if(symptom.substring(0,2).equals("백신")){
+            String[]words = symptom.split(" ");
+
+            for(int i =1; i<words.length-1;i+=2){
+                String vName = words[i];
+                int vNum = Integer.parseInt(words[i+1].substring(0,1));
+                if(Main.VaccinationMgr.find(vName)!=null){
+                    patient.getVaccinationList().put(vName+" "+vNum+"차",date);
+                }
+                else {
+                    System.out.println("해당되는 백신은 없습니다.");
+                }
+            }
+            Main.receptionMgr.getMlist().add(this);
         }
     }
 
@@ -57,9 +80,9 @@ public class Reception implements Manageable, UIData {
         texts[0] = date;
         texts[1] = name;
         texts[2] = gender;
-        texts[3] = ""+patient.age;
+        texts[3] = ""+patient.getAge();
         texts[4] = symptom;
-        texts[5] = doctor.name;
+        texts[5] = doctor.getName();
         return texts;
     }
 
@@ -68,49 +91,30 @@ public class Reception implements Manageable, UIData {
         if(patient.age <=3)
         {
             System.out.format("[%s] %s(%s, 만 %d세(%d개월)) : %s 담당의사 %s\n",
-                    date, name, patient.gender, patient.age, patient.month, symptom, doctor.name);
+                    date, name, patient.getGender(), patient.getAge(), patient.getMonth(), symptom, doctor.getName());
         }
         else {
             System.out.format("[%s] %s(%s, 만 %d세) : %s 담당의사 %s\n",
-                    date, name, patient.gender, patient.age, symptom, doctor.name);
+                    date, name, patient.getGender(), patient.getAge(), symptom, doctor.getName());
         }
     }
+
     public void printR() {
-        System.out.printf("(%s) %s : 담당의사 %s\n\t", date, symptom, doctor.name);
+        System.out.printf("(%s) %s : 담당의사 %s\n\t", date, symptom, doctor.getName());
     }
+
     @Override
     public boolean matches(String kwd) {
         if (kwd.equals(name))
             return true;
-        if (kwd.equals(""+patient.age))
+        if (kwd.equals("" + patient.getAge()))
             return true;
         if (kwd.equals(gender) && kwd.length() == 1)
             return true;
-        if (kwd.equals(doctor.name))
+        if (kwd.equals(doctor.getName()))
+            return true;
+        if (symptom.contains(kwd))
             return true;
         return false;
     }
 }
-
-/*
-    if (symptom.contains(kwd))
-            return true;
-    - 증상 키워드에 따라 출력하는 기능 필요시 추가
-    if kwd="예방접종" kwd 따라 증상에 예방접종 저장되어있는 patient 출력
-    */
-
-
-/*
-        name = patient.name;
-        age = patient.age;
-        gender = patient.gender;
-        symptom = scan.nextLine();
-        String kwd2 = scan.next();
-        for(Doctor doctor :hospitalMain.doctorMgr.mList){
-            if(doctor.matches(kwd2))
-                if(doctor.patientList.contains(patient)){
-                    this.doctor = doctor;
-                    break;
-                }
-        }
- */
