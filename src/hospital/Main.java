e70f84cf1a8957250c3f9d3b9d6309dfc073cf8f
HeeYeon-Kim <62700196+HeeYeon-Kim@users.noreply.github.com>
2022-11-22 05:45:21 +0900

    Update Main.java

--- src/hospital/Main.java
+++ src/hospital/Main.java
@@ -1,5 +1,6 @@
 package hospital;
 
+import java.util.Collections;
 import java.util.Scanner;
 
 import mgr.Factory;
@@ -19,11 +20,18 @@ public class Main {
 	static Manager patientMgr = new Manager();
 	static Manager receptionMgr = new Manager();
 	static Manager reservationMgr = new Manager();
+	static Manager VaccinationMgr = new Manager();
+
 	public void run()
 	{
 		doctorMgr.readAll("doctor.txt",new Factory(){
 			public Manageable create(){
 				return new Doctor();
+			}
+		});
+		VaccinationMgr.readAll("vaccination.txt",new Factory(){
+			public Manageable create(){
+				return new Vaccination();
 			}
 		});
 		patientMgr.readAll("patient.txt",new Factory(){
@@ -36,13 +44,25 @@ public class Main {
 				return new Reception();
 			}
 		});
+
+
+		reservationMgr.readAll("Reservation.txt",new Factory(){
+			public Manageable create(){
+				return new Reservation();
+			}
+		});
 		System.out.println("\n================= 전체 의사 리스트 =================");
 		doctorMgr.printAll();
 		System.out.println("\n================= 전체 환자 리스트 =================");
 		patientMgr.printAll();
 		System.out.println("\n=============== 전체 접수 리스트 =================");
 		receptionMgr.printAll();
+		System.out.println("\n=============== 백신 전체 리스트 =================");
+		VaccinationMgr.printAll();
+		System.out.println("\n=============== 전체 접수(예약 포함) 리스트 =================");
+		receptionMgr.printAll();
 	}
+
 
 	public static void main(String args[]) {
 		Main m = new Main();
